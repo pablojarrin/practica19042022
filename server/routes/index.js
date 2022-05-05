0679c252df4fb44819e99cb91846e9849eeab6a4
Jarrin Toscano <pjarrin@sigma-alimentos.com>
2022-05-05 18:21:44 -0500

    heroku

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,10 @@ const app = express.Router();
 
 
 app.use('/auth', require('./auth/login'))
-app.use('/producto', require('./producto/producto'))
+//app.use('/producto', require('./producto/producto'))
 app.use('/usuario', require('./usuario/usuario'));
 app.use('/empresa', require('./empresa/empresa'));
-app.use('/permisos/api', require('./permisos/api'));
+//app.use('/permisos/api', require('./permisos/api'));
 app.use('/permisos/rol', require('./permisos/rol'));
 app.use('/imagen', require('./imagen/imagen'));
 
